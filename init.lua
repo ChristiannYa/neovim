8806bead1f4d8c16925c91254c5068fbc96e98f8
ChristiannYa <christian.ylopez02@gmail.com>
2026-09-16 14:53:13 -0500

    fix(godot): tscn autocmd callback

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ require("diagnostics")
 vim.filetype.add({ extension = { tscn = "tscn" } })
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "tscn",
-    once = true,
     callback = function()
         require("godot.tscn").setup(vim.api.nvim_get_current_buf())
     end,
